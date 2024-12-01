e68fa879352d6c3758765871860f62c5a9a742b8
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-12-01 15:03:54 -0500

    day 1 - part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,14 @@
+from collections import Counter
+
 def main() :
     input = open('input.txt', 'r')
 
     left_pairs, right_pairs = get_pairs_from_input(input)
 
-    distance = get_distance_from_pairs(left_pairs, right_pairs)
-
-    print(distance)
+    # distance = get_distance_from_pairs(left_pairs, right_pairs)
+    # print(distance)
+    similarity = get_similarity_score(left_pairs, right_pairs)
+    print(similarity)
 
 def get_pairs_from_input(input):
     left_side = []
@@ -29,6 +32,18 @@ def get_distance_from_pairs(left, right):
         distance += abs(id_1-id_2)
 
     return distance
+
+# Gets the answer for part 2
+def get_similarity_score(left, right):
+    similarity = 0
+    counter = Counter(right)
+
+    for id in left:
+
+        if id in counter:
+            similarity += id * counter[id]
+
+    return similarity
 
 if __name__ == "__main__":
     main()
